0a6e42d9884e354f7b58589aea004bf1d92ccd25
GuestJim <mlcforeigner@gmail.com>
2022-03-17 14:01:45 -0400

    Move to Environment instead of CSV
    Instead of using a CSV for storing all of the data, using saveRDS and an environment allows the formatting of a data frame to be saved and loaded as well. The CSV will still be made, but using the environment will likely be better in the long run.
    It is easy to go back though.

--- CPU Thermal - Input.r
+++ CPU Thermal - Input.r
@@ -32,28 +32,31 @@ if (interactive())	{
 	pdf(NULL)
 }
 
-if (!file.exists("Combined.csv.bz2"))	{
+FILE	=	paste0(DATA$TESTname, " ~ ", DATA$COOLERname)
+
+if (!file.exists(	paste0(FILE, ".env")	))	{
 	hold	=	new.env()
 	# if	(grepl("AMD", CPUname))		source("~CPU Thermal - Data - AMD.r",	local = hold)
 	# if	(grepl("Intel", CPUname))	source("~CPU Thermal - Data - Intel.r",	local = hold)
 	if	(file.exists("~CPU Thermal - Data - AMD.r"))		source("~CPU Thermal - Data - AMD.r",	local = hold)
 	if	(file.exists("~CPU Thermal - Data - Intel.r"))		source("~CPU Thermal - Data - Intel.r",	local = hold)
 	#	with the hold environment, everything the Data scripts do is placed into hold, but dataALL will be placed in Global, so hold can be removed at the end
+	DATA$dataALL	=	dataALL
+	saveRDS(DATA, paste0(FILE,".env"), compress="bzip2")
 	write_csv(dataALL, "Combined.csv.bz2")
+	
 	rm(hold)
 }	else	{
-	dataALL	=	read_csv("Combined.csv.bz2", guess_max = 10, lazy = TRUE, show_col_types = FALSE)
+	# dataALL	=	read_csv("Combined.csv.bz2", guess_max = 10, lazy = TRUE, show_col_types = FALSE)
+	# dataALL$Thread	=	ordered(dataALL$Thread)
+	# dataALL$Core	=	ordered(dataALL$Core)
+	# dataALL$Socket	=	ordered(dataALL$Socket)
+	# dataALL$Period	=	ordered(dataALL$Period, levels = levsPER)
+	# DATA$dataALL	=	dataALL
+	
+	DATA	=	readRDS(paste0(FILE, ".env"))
+	assign("dataALL", DATA$dataALL, envir = .GlobalEnv)
+	if (!file.exists("Combined.csv.bz2"))	write_csv(dataALL, "Combined.csv.bz2")
 }
-
-dataALL$Thread	=	ordered(dataALL$Thread)
-dataALL$Core	=	ordered(dataALL$Core)
-dataALL$Socket	=	ordered(dataALL$Socket)
-dataALL$Period	=	ordered(dataALL$Period, levels = levsPER)
-dataALL$CPU		=	ordered(dataALL$CPU)
-dataALL$Cooler	=	ordered(dataALL$Cooler)
-dataALL$Test	=	ordered(dataALL$Test)
-
-DATA$dataALL	=	dataALL
-saveRDS(DATA, "DATA.env", compress="bzip2")
 
 source("@CPU Thermal - Output.r")
